7e6ecfe67bf153fd2237b132949e3e5206ef20e5
IlhamliElshad <ilhamli456@gmail.com>
2023-09-22 22:07:30 +0400

    admin panelin guncellenmesi.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from blog.models import Post
 
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status']
+    search_fields = ['title','body']
+    date_hierarchy = 'publish'
+    ordering = ['created']
+    prepopulated_fields = {"slug":('title',)}
+    raw_id_fields = ['author']
