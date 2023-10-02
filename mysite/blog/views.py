3e49c697447e4d1eb29ed38020c54385ec3b99c6
IlhamliElshad <ilhamli456@gmail.com>
2023-10-02 20:58:06 +0400

    hem templatehem  de views  wlave etdik..

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post 
 # Create your views here.
+
+
+def post_list(request):
+    posts =Post.published.all()
+    return render(request,'blog/post/list.html', {"posts":posts})
+
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post,pk=pk,status='PB')
+    return render(request, 'blog/post/detail.html',{"post":post})
